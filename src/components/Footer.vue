5be5409aac44d0fc48be22adc9ec0c397913e233
ktagbo <kingsleytagbo@gmail.com>
2020-09-11 11:07:18 -0500

    Vue Blog

---
+++ src/components/Footer.vue
@@ -0,0 +1,26 @@
+<template>
+<h1> Footer ... </h1>
+</template>
+
+<script>
+
+export default {
+  name: "FooterItem",
+  data() {
+    return {
+    };
+  },
+
+  mounted() {
+  },
+
+  computed: {
+  },
+
+  methods: {
+  }
+};
+</script>
+
+<style>
+</style>
