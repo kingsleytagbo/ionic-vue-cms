ba926c65fcc538efc757c5051de243909555dfeb
ktagbo <kingsleytagbo@gmail.com>
2020-10-06 12:57:04 -0500

    Vue 3.0.0

---
+++ src/components/Header.vue
@@ -0,0 +1,53 @@
+<template>
+<div class="fixed-top sticky-top">
+        <div class="navbar navbar-expand-md navbar-dark bg-dark">
+            <a class="navbar-brand" href="/">
+                <!-- <img src="http://cdn1.launchfeatures.com/shared/images/favicon.ico" width="21" height="32" class="d-inline-block align-top" title="Home"> -->
+                <i class="fa fa-star-o"></i>
+                Vue @Next
+            </a>
+
+            <!-- BEGIN SEARCH -->
+            <button @click="toggleSearch()" class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarMenu" aria-controls="navbarmenu" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+
+            <div v-if="showSearch" class="navbar-collapse collapse show" id="navbarMenu" style="">
+                <form class="form-inline my-0 my-lg-0" method="get" action="/">
+                    <input name="search" id="search" class="form-control form-control-sm mr-sm-2" type="search" placeholder="Search Vue @Next" aria-label="Search">
+                    <button class="btn btn-outline-light form-control-sm my-0 my-sm-0" type="button">Search</button>
+                </form>
+            </div>
+            <!-- END SEARCH -->
+
+        </div>
+
+</div>
+</template>
+
+<script>
+
+export default {
+  name: "Header",
+  data() {
+    return {
+         showSearch: true
+    };
+  },
+
+  mounted() {
+  },
+
+  computed: {
+  },
+
+  methods: {
+      toggleSearch(){
+          this.showSearch = !this.showSearch;
+      }
+  }
+};
+</script>
+
+<style>
+</style>
