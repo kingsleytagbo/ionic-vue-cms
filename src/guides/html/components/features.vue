170fd9f7aa73c0e063f62fa3f88abef77824bbdd
kingsleytagbo@gmail.com <kingsleytagbo@gmail.com>
2020-11-27 19:38:52 -0600

    vuejs + no node / npm

--- src/guides/html/components/features.vue
+++ src/guides/html/components/features.vue
@@ -1,5 +1,8 @@
 <template>
+<div>
   <ol><li>Vue from your html/js. No node.js, no build step, no webpack.</li></ol>
+  <h6>... /src/guides/html/components/features.vue</h6>
+  </div>
 </template>
 
 <script>
