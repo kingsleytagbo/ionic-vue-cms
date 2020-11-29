ba96060d61f3669fc90772be587ad851b776e5bb
kingsleytagbo@gmail.com <kingsleytagbo@gmail.com>
2020-11-29 16:17:29 -0600

    Vue Components

--- src/guides/html/components/features.vue
+++ src/guides/html/components/features.vue
@@ -1,20 +1,18 @@
 <template>
-<div>
-  <ol><li>Vue from your html/js. No node.js, no build step, no webpack.</li></ol>
-  <h6>... /src/guides/html/components/features.vue</h6>
+  <div>
+    <ol>
+      <li>Vue Component.</li>
+    </ol>
   </div>
 </template>
 
 <script>
 module.exports = {
-  mounted () {
-    console.log('Mounted')
-  }
-}
+  mounted() {},
+};
 </script>
-
 <style scoped>
-  li {
-    background-color: yellow;
-  }
+li {
+  background-color: yellow;
+}
 </style>
