36350aa3efe2bb5d9832cfa20e670a6ff8c76f77
kingsleytagbo@gmail.com <kingsleytagbo@gmail.com>
2020-11-29 18:58:30 -0600

    Vue Components

---
+++ src/guides/html/components/featureone.vue
@@ -0,0 +1,22 @@
+<template>
+  <div></div>
+</template>
+
+<script>
+export default {
+  name: "feature-one",
+  data() {
+    return {
+      users: [],
+    };
+  },
+  methods: {
+    getUsers() {
+      return [];
+    },
+  },
+};
+</script>
+<style scoped>
+li {background-color: yellow;}
+</style>
